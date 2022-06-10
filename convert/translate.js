d477999c049861ad80dbab2c7b1669ed8df2a276
mccallc <26819445+mccallc@users.noreply.github.com>
2022-06-10 12:24:41 -0400

    implementing conversion for heal-specific fields

--- convert/translate.js
+++ convert/translate.js
@@ -1,36 +1,41 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var validator = require('is-my-json-valid')
-
-var schema_url;
-var url = "https://data.stage.qdr.org/api/datasets/:persistentId/versions/:draft?persistentId=";
-var pid = "";
-var token = "";
-
-var metadata;
-var getJSON = function(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    //xhr.setRequestHeader('X-Dataverse-Key', a_token);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        metadata = JSON.parse(xhr.responseText);
-      } else {
-        console.log('Something went wrong: ' + xhr.status);
-      }
-    };
-    xhr.send();
-};
-
-getJSON(url + pid, token)
-
-heal = metadata.data.metadataBlocks.heal.fields
-
-//for (let i=0, l=heal.length; i<l; i++) {
-
-//}
-
+// Load packages
+var validator = require('is-my-json-valid');
 var empty = require('json-schema-empty').default;
-getJSON("https://raw.githubusercontent.com/HEAL/heal-metadata-schemas/main/study-level-metadata-schema/schema-clean/json/study-metadata-schema.json")
-empty(metadata)
+
+// Load json data (will eventually be implemented as APIs)
+const schema = require('./study-metadata-schema.json');
+const dataverse = require('./dataverse_example.json');
+
+// create template and begin extracting data
+var template = empty(schema);
+var heal = dataverse.data.metadataBlocks.heal.fields;
+var citation = dataverse.data.metadataBlocks.citation.fields;
+
+// cycle through two levels and assign values into the empty template
+for (var i=0; i<heal.length; i++) {
+  var toplevel = heal[i]["typeName"];
+  // had to name the field as heal_citation due to a limitation of dataverse
+  // we change it back here
+  if (toplevel=="heal_citation") {
+    toplevel = "citation";
+  }
+
+  var sublevel = heal[i]["value"];
+  // need to extract into a simple name:var format
+  for (let key in sublevel) {
+    sublevel[key] = sublevel[key]["value"];
+  }
+
+  // commit back to empty template based on "typeName"
+  template[toplevel] = sublevel;
+}
+
+// data that got merged into standard dataverse categories
+// requires manual handling
+var citation_map = new Object;
+for (var i=0; i<citation.length; i++) {
+  citation_map[citation[i]["typeName"]] = citation[i]["value"];
+}
+template.minimal_info["study_name"] = citation_map.title;
+template.minimal_info["study_description"] = citation_map.dsDescription[0]["dsDescriptionValue"]["value"];
+templatecitation_map.author[0]["authorName"]["value"]
